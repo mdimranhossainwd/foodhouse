1b88f0b1d9e26bb371ce8939659c402d3df4ca54
mdimranhossainwd <mdimranhossain7498@gmail.com>
2024-03-03 11:57:49 +0600

    Testimonials Sections Completed

---
+++ src/Shared/Banner/TestimonialBanner.jsx
@@ -0,0 +1,50 @@
+import SubHeading from "../Heading/SubHeading";
+
+const TestimonialBanner = () => {
+  return (
+    <div
+      className="min-h-[480px] my-12 flex items-center justify-center"
+      style={{
+        backgroundImage:
+          "url(https://images.unsplash.com/photo-1543353071-873f17a7a088?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
+      }}
+    >
+      <div>
+        <SubHeading heading="Testimonials" subHeading="What Our Clients Say " />
+        <div className="mb-10">
+          <figure className="max-w-screen-md mx-auto text-center">
+            <svg
+              className="w-10 h-10 mx-auto mb-3 text-gray-400 dark:text-gray-600"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 18 14"
+            >
+              <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z" />
+            </svg>
+            <blockquote>
+              <p className="text-2xl italic font-medium text-gray-900 ">
+                "Flowbite is just awesome. It contains tons of predesigned
+                components and pages starting from login screen to complex
+                dashboard. Perfect choice for your next SaaS application."
+              </p>
+            </blockquote>
+            <figcaption className="flex items-center justify-center mt-6 space-x-3 rtl:space-x-reverse">
+              <img
+                className="w-14 h-14 rounded-full"
+                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png"
+                alt="profile picture"
+              />
+              <div className="flex items-center divide-x-2 rtl:divide-x-reverse ">
+                <cite className="pe-3 font-medium text-lg">Michael Gough</cite>
+                <cite className="ps-3 text-sm text-[#444]">CEO at Google</cite>
+              </div>
+            </figcaption>
+          </figure>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TestimonialBanner;
--- src/pages/HomePages.jsx
+++ src/pages/HomePages.jsx
@@ -3,6 +3,7 @@ import Chef from "../Components/Chef/Chef";
 import NewsLetter from "../Components/Newsletter/NewsLetter";
 import Banner from "../Shared/Banner/Banner";
 import SubBaner from "../Shared/Banner/SubBaner";
+import TestimonialBanner from "../Shared/Banner/TestimonialBanner";
 const HomePages = () => {
   return (
     <div>
@@ -11,6 +12,7 @@ const HomePages = () => {
       <Chef />
       <SubBaner />
       <NewsLetter />
+      <TestimonialBanner />
     </div>
   );
 };
