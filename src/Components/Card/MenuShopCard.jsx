194dba663a5ce3abe1dec0b42755d1e47aa9a574
mdimranhossainwd <mdimranhossain7498@gmail.com>
2024-03-14 06:40:05 +0600

    Add to Item Dashboard Cart Page

--- src/Components/Card/MenuShopCard.jsx
+++ src/Components/Card/MenuShopCard.jsx
@@ -1,5 +1,37 @@
+import { useContext } from "react";
+import Swal from "sweetalert2";
+import { AuthContext } from "../../Provider/AuthProvider";
+import useAxios from "../../hooks/useAxios";
+
 const MenuShopCard = ({ menu }) => {
   const { _id, str_Name, str_Thumb, str_Introduction, str_Prize } = menu || {};
+  const { user } = useContext(AuthContext);
+  const axios = useAxios();
+  const handleAddtoCart = () => {
+    console.log("User", user.email);
+    if (user && user?.email) {
+      const addItem = {
+        menuID: _id,
+        str_Name,
+        email: user?.email,
+        str_Thumb,
+        str_Prize,
+      };
+
+      axios.post("/additem", addItem).then((res) => {
+        console.log(res.data);
+        if (res.data) {
+          Swal.fire({
+            title: `${str_Name}`,
+            text: "Food is Added to The Cart",
+            icon: "success",
+            showConfirmButton: false,
+          });
+        }
+      });
+    }
+  };
+
   return (
     <div>
       <div className="bg-[#F3F3F3] border " data-aos="fade-up">
@@ -27,7 +59,10 @@ const MenuShopCard = ({ menu }) => {
 
           <p className="mb-3 font-normal text-[#151515] ">{str_Introduction}</p>
 
-          <button className="bg-[#E8E8E8] px-5 text-md space-x-2 font-medium py-3 border-b-4 border-[#F42643] text-[#F42643] border-0 rounded-md">
+          <button
+            onClick={handleAddtoCart}
+            className="bg-[#E8E8E8] hover:bg-[#F42643] px-5 text-md space-x-2 font-medium py-3 border-b-4 border-[#F42643] hover:border-[#fff] hover:text-white transition text-[#F42643] border-0 rounded-md"
+          >
             ADD TO CART
           </button>
         </div>
